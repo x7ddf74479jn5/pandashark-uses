80a9ba7102e36965bd7103c8b7e01848c2136ac3
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-10-11 02:43:26 +0900

    :adhesive_bandage: chore: update website info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Pandashark Blog'
+export const SITE_DESCRIPTION = 'Welcome to my blog!'
+export const HOMEPAGE_URL = 'https://three-homepage.vercel.app/'

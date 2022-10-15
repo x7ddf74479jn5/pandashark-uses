be7cbc9ffd5ba92846f19261ea8b2c4347609517
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-10-15 14:20:13 +0900

    💬 fix: site name and description

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Pandashark Blog'
-export const SITE_DESCRIPTION = 'Welcome to my blog!'
+export const SITE_TITLE = 'What I use - Pandashark'
+export const SITE_DESCRIPTION = 'A curated list of the tools I use'
 export const HOMEPAGE_URL = 'https://three-homepage.vercel.app/'
